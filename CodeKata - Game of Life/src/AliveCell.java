67025131cc391000c26b25cc9b0f36fa7e6067a8
Rachael <rachael@bernkastel.co.uk>
2014-11-22 11:43:04 +0000

    First go at creating Cell class.

--- CodeKata - Game of Life/src/AliveCell.java
+++
@@ -1,4 +0,0 @@
-
-public class AliveCell implements Cell {
-
-}
--- CodeKata - Game of Life/src/Cell.java
+++ CodeKata - Game of Life/src/Cell.java
@@ -1,4 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public interface Cell {
+public class Cell {
+
+	private List<Cell> neighbours = new ArrayList<Cell>();
+	private CellState cellState;
+
+	public Cell(CellState state) {
+		cellState = state;
+	}
+
+	public CellState getState() {
+		return cellState;
+	}
+
+	public void checkNeighbours() {
+		int alive = 0;
+		for (Cell neighbour : neighbours) {
+			if (neighbour.getState().equals(CellState.ALIVE)) {
+				alive++;
+			}
+		}
+		applyRules(alive);
+	}
+
+	private void applyRules(int alive) {
+		if (cellState.equals(CellState.ALIVE)) {
+			if (alive < 2 || alive > 3) {
+				cellState = CellState.DEAD;
+			}
+		} else {
+			if (alive == 3) {
+				cellState = CellState.ALIVE;
+			}
+		}
+	}
+	
+	public void setNeighbour(Cell neighbour) {
+		neighbours.add(neighbour);
+	}
 
 }
